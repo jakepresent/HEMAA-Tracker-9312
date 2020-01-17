cd68ce38e9efa607dfd1b300ad37bf2c15c6b4ed
Ben Lashley <blashley3@gatech.edu>
2020-01-17 18:37:21 +0000

    First connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const express = require('express');
 var cors = require('cors');
-const bodyParser = require('body-parser');
-const logger = require('morgan');
 const Data = require('./data');
 
 const API_PORT = 3000;
@@ -11,10 +9,18 @@ const app = express();
 app.use(cors());
 const router = express.Router();
 
-const uri = "mongodb+srv://hemaa123:<password>@hemaamembershipchecker-z2mun.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+const uri = "mongodb+srv://hemaa123:NuN1v4XAHn9oGpgH@hemaamembershipchecker-z2mun.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db = mongoose.connection;
+
+db.once('open', () => console.log('connected to the database'));
+
+// checks if connection with the database is successful
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// append /api for our http requests
+app.use('/api', router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
