217ef6b7ee66dc3c9f1a51b7dadccb7cb2bcc953
Ben Lashley <blashley3@gatech.edu>
2020-01-28 15:21:02 -0500

    Added color coding for console strings

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
+const chalk = require("chalk");
 const uri = require("./config/keys").databaseURI;
 
+// Configurations
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+// Open connection
 mongoose
-  .connect(uri, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB successfully connected"))
+  .connect(uri)
+  .then(() => console.log(chalk.green("MongoDB successfully connected")))
   .catch(err => console.log(err));
 
 db = mongoose.connection;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./database");
+const chalk = require("chalk");
 const API_PORT = 3001;
 const memberRouter = require("./routes/members");
 
@@ -18,4 +19,14 @@ app.use(bodyParser.json());
 app.use(`/api/member`, memberRouter);
 
 // Launch backend
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+var server = app.listen(API_PORT, () => console.log(chalk.cyan(`LISTENING ON PORT ${API_PORT}`)));
+
+// Close backend
+process.on("SIGINT", function() {
+  db.close(function() {
+    console.log(chalk.magentaBright("Database connection closed due to application termination"))
+  });
+  server.close(function() {
+    console.log(chalk.magentaBright("Server closed due to application termination"));
+  });
+});
