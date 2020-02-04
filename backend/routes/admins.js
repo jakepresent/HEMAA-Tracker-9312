80678b6e193563d5a8100e8eaef786b45eabe58b
Ben Lashley <blashley3@gatech.edu>
2020-02-04 14:41:40 -0500

    Added skeleton code for admin login authentication

---
+++ backend/routes/admins.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const AdminController = require("../models/Admin");
+
+// router.post(`/login`, AdminController.loginAdmin);
+
+module.exports = router;
