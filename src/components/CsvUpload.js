a87c56a2ce7802a183672d3f53d4e8a6579cd957
Jake Present <jake.present@codemettle.com>
2020-02-12 13:54:59 -0500

    Added file upload to csvupload

--- src/components/CsvUpload.js
+++ src/components/CsvUpload.js
@@ -7,7 +7,18 @@ const api = require("../api");
 
 const CsvUpload = () => (
   <div>
-    <p>Upload CSV here</p>
+    <br />
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="inputGroupFileAddon01">Upload</span>
+      </div>
+      <div class="custom-file">
+        <input type="file" accept=".csv" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01" />
+        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+      </div>
+    </div>
+
+
   </div>
 
 );
