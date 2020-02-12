74f8b384d909c0b0e3f72e328f8f7674d1e59a41
Ben Lashley <blashley3@gatech.edu>
2020-02-12 12:15:49 -0500

    Implemented admin authentication

--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const AdminController = require("../controllers/adminController");
 
-router.post(`/create`, AdminController.createAdmin);
-// router.post(`/login`, AdminController.loginAdmin);
+router.post(`/create/`, AdminController.createAdmin);
+router.post(`/login/`, AdminController.getAdminByEmailandPassword);
 
 module.exports = router;
